4b65a1c5839784c0cfd3e42b42bd27e950798600
Victor Rodrigues <vr33209@gmail.com>
2019-08-06 17:08:02 -0300

    Configurando layout e validações de input

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
+import { Link } from "react-router-dom";
+
+import Logo from "~/assests/logo.svg";
 // import { Container } from './styles';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um email valido !")
+    .required("E-mail é obrigatorio !"),
+  password: Yup.string().required("Senha é obrigatorio!")
+});
 export default function Signin() {
-  return <h1>Signin</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
-// import { Container } from './styles';
+import { Link } from "react-router-dom";
 
-export default function Signup() {
-  return <h1>Signup</h1>;
+import Logo from "~/assests/logo.svg";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um email valido !")
+    .required("E-mail é obrigatorio !"),
+  name: Yup.string()
+    .email("Insira um email valido !")
+    .required("Nome é obrigatorio !"),
+  password: Yup.string()
+    .required("Senha é obrigatorio!")
+    .min(6, "No minimo 6 caracteres!")
+});
+
+export default function Signin() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">já possuo conta</Link>
+      </Form>
+    </>
+  );
 }
